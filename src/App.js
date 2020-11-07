e31633f893881bb0f8ac5684d53b409bba3de093
seyeon <tltptltptltp@gmail.com>
2020-11-07 23:31:23 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from "react";
-import { HashRouter, Route} from "react-router-dom";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Detail from "./routes/Detail";
-import Navigation from "./components/Navigation";
-import "./App.css";
+import Header from "./Components/Header";
+import Router from "./Components/Router";
+import React , { Component } from "react";;
 
-function App(){
-  return (
-    <HashRouter>
-      <Navigation/>
-        <Route path="/" exact={true} component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/movie/:id" component={Detail}/>
-    </HashRouter>
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Header />
+      <Router />
+      </>
     );
+  }  
 }
 
 export default App;
